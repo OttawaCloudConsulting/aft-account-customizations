6346761fa88546f2dbd3d4eaef5100be12edbbc6
OCC <cturner@ottawacloudconsulting.com>
2026-01-05 20:58:36 -0500

    debug output checking for bucket

--- baseline/api_helpers/shell_scripts/cdk-bootstrap.sh
+++ baseline/api_helpers/shell_scripts/cdk-bootstrap.sh
@@ -26,6 +26,23 @@ echo "Automation Account: ${AFT_MGMT_ACCOUNT}"
 echo "Target Account: ${VENDED_ACCOUNT_ID}"
 echo "Region: ${AWS_DEFAULT_REGION}"
 
+# Check for existing of CDK Assets Bucket
+BUCKET_NAME="cdk-hnb659fds-assets-${VENDED_ACCOUNT_ID}-${AWS_DEFAULT_REGION}" && \
+echo "Checking for bucket: ${BUCKET_NAME}" && \
+BUCKET_EXISTS=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name == "'${BUCKET_NAME}'") | .Name') && \
+if [ -n "$BUCKET_EXISTS" ]; then
+  echo "Bucket exists: ${BUCKET_EXISTS}"
+  echo "Counting objects..."
+  OBJECT_COUNT=$(aws s3 ls s3://${BUCKET_NAME} --recursive | wc -l | xargs)
+  echo "Total objects in bucket: ${OBJECT_COUNT}"
+#   if [ "$OBJECT_COUNT" -eq 0 ]; then
+#     echo "Bucket is empty. Deleting left over bucket from previous deployment..."
+#     aws s3api delete-bucket --bucket ${BUCKET_NAME} | jq
+#   fi
+else
+  echo "Bucket does not exist"
+fi
+
 # Bootstrap CDK with trust to AFT automation account
 cdk bootstrap \
   --trust "${AFT_MGMT_ACCOUNT}" \
